72d65e05be173f8c0a412fe0371913edc51a9d97
thelazycat-0728 <jonathanyeo0728@gmail.com>
2026-04-15 22:45:05 +0800

    feat: added GUI and add car functionality to Car Dealer Agent

--- src/agents/DealerAgent.java
+++ src/agents/DealerAgent.java
@@ -1,12 +1,31 @@
 package agents;
 import jade.core.Agent;
+import jade.core.behaviours.*;
+import gui.CarDealerGui;
+import java.util.*;
 
 public class DealerAgent extends Agent{
+
+  private Hashtable<String, Integer> catalogue = new Hashtable<>();
+
+  private CarDealerGui myGui;
+
   protected void setup() {
-    System.out.println("Hello world!");
+    myGui = new CarDealerGui(this);
+    myGui.display();
   }
 
   protected void takeDown() {
     System.out.println("Bye world!");
+    myGui.dispose();
+  }
+
+  public void updateCatalogue(final String carModel, final int price) {
+    addBehaviour(new OneShotBehaviour() {
+      public void action() {
+        catalogue.put(carModel, price);
+        System.out.println(carModel+" inserted into catalogue. Price = "+price);
+      }
+    });
   }
 }
